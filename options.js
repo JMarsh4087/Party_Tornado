90f5d9ee22371f871c82a4c73043737cc70a9263
JMarsh4087 <8380168+JMarsh4087@users.noreply.github.com>
2025-06-05 21:03:22 -0400

    Add Gifs

--- options.js
+++ options.js
@@ -4,12 +4,14 @@ const OPTIONS = [
   "Timeguessr",
   "Quiplash",
   "Scattergories",
+  "Crossword",
   "Golf with Friends",
   "Trivia Murder Party",
   "Code Names",
   "Drawful",
   "Blather Round",
-  "Turbo Golf Racing"
+  "Turbo Golf Racing",
+  "Try The New Thing Someone Just Suggested!"
 ];
 
 console.log("OPTIONS initialized:", OPTIONS);
--- script.js
+++ script.js
@@ -56,9 +56,20 @@ function renderOptions() {
 
 const gifLinks = [
   "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjV2eXFua3J4OW14ZHlubnA2ejlpMmhuaXJhdzgyM2JtMTMycHFtaCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/d3mlXPjoK1ROfr9u/giphy.webp",
-  "https://media2.giphy.com/media/l3q2K5jinAlChoCLS/giphy.gif",
-  "https://media1.giphy.com/media/SKGo6OYe24EBG/giphy.gif",
-  "https://media3.giphy.com/media/RrVzUOXldFe8M/giphy.gif"
+  "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjV2eXFua3J4OW14ZHlubnA2ejlpMmhuaXJhdzgyM2JtMTMycHFtaCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/MXvDhlmD0eB5qNvvjZ/giphy.webp",
+  "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjV2eXFua3J4OW14ZHlubnA2ejlpMmhuaXJhdzgyM2JtMTMycHFtaCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3o7aCRvp4PHhmTUtMY/200.webp",
+  "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjV2eXFua3J4OW14ZHlubnA2ejlpMmhuaXJhdzgyM2JtMTMycHFtaCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/ensH2FRpbq4V9WIoCM/200.webp",
+  "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWo1OGJ3a2ZmMjZsb2g1ZXdtcDU3M2xva2VyZGJnaXphZ2FjbWpsMyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/68FsmDsSBACTC/200.webp",
+  "https://media2.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3NzJoODRndW5lOWh5c3B0d2c0bHZ4YTZnNzhsYmVjaTJqem1hNGswcCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/l0G16Vajk8KdqsC3e/200.webp",
+  "https://media2.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3NzJoODRndW5lOWh5c3B0d2c0bHZ4YTZnNzhsYmVjaTJqem1hNGswcCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/t0PGq0CLQMXba/giphy.webp",
+  "https://media4.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3Zmx4ajRkMDM3eGc4bHNha2Z3ZGRnb2U0dzhvYmhqcmZ6bnBzbHh4dCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/HhhajSmRfikXC/giphy.webp",
+  "https://media4.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3Zmx4ajRkMDM3eGc4bHNha2Z3ZGRnb2U0dzhvYmhqcmZ6bnBzbHh4dCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/uFdss38UliWR2/giphy.webp",
+  "https://media0.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bDVzMW85aHZpMTQ5cWJ1YjY4bXExZTVra3Q1bnR1a2ZiZGloMmE2eCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/7nE3w2AXGslyg/200.webp",
+  "https://media1.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3eGNqeDl5aDYxMnh3dmwwamRxMXFid3RvN2xzYXZmcWxrYnNwN3ZqayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/er5dUr5XWSlH2x33Fr/200.webp",
+  "https://media4.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3cGIzY3c3dG4yY21qYTJkOWM1bHVwdjdtN3RsZ3ZzdW1leHVybG9pOSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3UreUb6rZ7kcB0KlPX/200.webp",
+  "https://media4.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3cHB3NmJqZW5xenBkN21rMXE0aWZmMTg5azY5eWJpM3JubTBvYm9mayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/4OMBMoJTOdF0vF4RVf/200.webp",
+  "https://media4.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3cHB3NmJqZW5xenBkN21rMXE0aWZmMTg5azY5eWJpM3JubTBvYm9mayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/cOEHZYZ9l2H182HQbY/giphy.webp",
+  "https://media3.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bXpkYXdva2FrZDZkandmMGYybWg4MDUxdGEwN3F4dWY3Z3dza3J1NiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/nM6a5kDV8JhTccIx0g/200.webp"
 ];
 
 function renderRandomGif() {
@@ -153,7 +164,7 @@ database.ref("votes").on("value", (snapshot) => {
   pickBtn.onclick = () => {
     const choiceIndex = pickWeightedRandom(scores);
     if (choiceIndex !== null) {
-      finalChoiceDiv.innerHTML = `<h3>Winner:</h3><p>${OPTIONS[choiceIndex]}</p>`;
+      finalChoiceDiv.innerHTML = `<h3>🏆Winner:</h3><p>${OPTIONS[choiceIndex]}</p>🏆`;
     } else {
       finalChoiceDiv.innerHTML = `<p>No votes to pick from.</p>`;
     }
