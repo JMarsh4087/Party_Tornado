ce8aedfe642271e41a17ef7876f0c299f58fbeaf
JMarsh4087 <8380168+JMarsh4087@users.noreply.github.com>
2025-06-21 00:04:44 -0400

    Update options.js

--- options.js
+++ options.js
@@ -2,21 +2,23 @@ console.log("Loading options.js...");
 
 const OPTIONS = [
   "🕰️🌎❓Timeguessr🕰️🌎❓",
-  "🤡Quiplash🤡",
   "✅Scattergories🚫",
   "👑🏰King of the Castle👑🏰",
+  "🕵️‍♂️Code Names🕵️‍♀️",
   "➡️⬇️Crossword➡️⬇️",
   "⛳🏌️Golf with Friends⛳🏌️",
   "🧦👨‍⚖️Guilty As Sock🧦👩‍⚖️",
+  "🎥👻Content Warning🎥👻",
   "✏️🎨Drawful✏️🎨",
+  "🤡Quiplash🤡",
   "🎊🎮What the Pak🎊🎮?",
   "🤷‍♂️Blather Round🤷‍♂️",
+  "🤿🌊Murky Divers🤿🌊",
+  "❓Ryan picks a random quiz❓",
   "🍞🥦🍉🛒Supermarket Together🍞🥦🍉🛒",
   "⛳🏎️🏁Turbo Golf Racing⛳🏎️🏁",
   "⁉️🔪🩸🎉Trivia Murder Party⁉️🔪🩸🎉",
   "⚔️🛡️Valheim⚔️🛡️",
-  "🕵️‍♂️Code Names🕵️‍♀️",
-  "🤿🌊Murky Divers🤿🌊",
   "✨✨✨Try The New Thing Someone Just Suggested!✨✨✨"
 ];
 
